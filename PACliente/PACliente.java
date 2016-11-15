b6d65cd8f6cd9f2bb7f616300e1080a3b8e44696
Bernardo Vieira <bernardo5@live.com.pt>
2016-11-15 13:06:28 +0000

    MulticastClients

--- PACliente/PACliente.java
+++ PACliente/PACliente.java
@@ -1,9 +1,32 @@
-
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
 
 public class PACliente
 {
-    public static void main(String[] args)
-    {
-        UdpClient udpclient = new UdpClient("127.0.0.1", 5007);
+    public static void main(String[] args) throws Exception {
+        int mcPort = 12345;
+        String mcIPStr = "230.1.1.1";
+        MulticastSocket mcSocket = null;
+        InetAddress mcIPAddress = null;
+        mcIPAddress = InetAddress.getByName(mcIPStr);
+        mcSocket = new MulticastSocket(mcPort);
+        System.out.println("Multicast Receiver running at:"
+            + mcSocket.getLocalSocketAddress());
+        mcSocket.joinGroup(mcIPAddress);
+
+        DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+
+        System.out.println("Waiting for a  multicast message...");
+        mcSocket.receive(packet);
+        String msg = new String(packet.getData(), packet.getOffset(),
+            packet.getLength());
+        System.out.println("[Multicast  Receiver] Received:" + msg);
+
+        mcSocket.leaveGroup(mcIPAddress);
+        mcSocket.close();
     }
+        
+        //UdpClient udpclient = new UdpClient("127.0.0.1", 5007);
+ 
 }
