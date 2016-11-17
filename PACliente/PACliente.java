5fcd51f52b1c42a019e6233cf66460b49cc0c61c
Bernardo Vieira <bernardo5@live.com.pt>
2016-11-17 14:24:05 +0000

    TCP[Server-Client]

--- PACliente/PACliente.java
+++ PACliente/PACliente.java
@@ -1,9 +1,15 @@
 
+
 public class PACliente
 {
     public static void main(String[] args) throws Exception {
-        //UdpClient udpclient = new UdpClient("127.0.0.1", 5007);
         
-        MulticastCC multicastCC = new MulticastCC();
+        //pergunta o nome de utilizador
+        //conectar à diretoria
+        //UdpClient udpclient = new UdpClient("127.0.0.1", 5007);
+        //retorna ip e porto do servidor
+        //conecta ao servidor
+        TcpToServer tcpClient = new TcpToServer("127.0.0.1", 5007);
+        //iniciar multicaste
     }
 }
