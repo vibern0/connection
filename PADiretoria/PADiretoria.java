78bd04399493eb970841daccabaaa4bb564d469a
Daniel Simões <Daniel Simes@192.168.1.71>
2016-11-20 00:32:46 +0000

    mais cenas

--- PADiretoria/PADiretoria.java
+++ PADiretoria/PADiretoria.java
@@ -8,23 +8,40 @@ import java.net.InetAddress;
 
 public class PADiretoria
 {
+    static String cmd;
+    
     public static void main(String[] args) throws Exception {
-        int mcPort = 12345;
-        String mcIPStr = "230.1.1.1";
-        DatagramSocket udpSocket = new DatagramSocket();
-
-        InetAddress mcIPAddress = InetAddress.getByName(mcIPStr);
-        DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
-        udpSocket.receive(packet);
-        byte[] msg = packet.getData();
-        packet = new DatagramPacket(msg, msg.length);
-        packet.setAddress(mcIPAddress);
-        packet.setPort(mcPort);
-        udpSocket.send(packet);
-
-        System.out.println("Sent a  multicast message.");
-        System.out.println("Exiting application");
-        udpSocket.close();
+//        int mcPort = 12345;
+//        String mcIPStr = "230.1.1.1";
+//        DatagramSocket udpSocket = new DatagramSocket();
+//
+//        InetAddress mcIPAddress = InetAddress.getByName(mcIPStr);
+//        DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
+//        udpSocket.receive(packet);
+//        byte[] msg = packet.getData();
+//        packet = new DatagramPacket(msg, msg.length);
+//        packet.setAddress(mcIPAddress);
+//        packet.setPort(mcPort);
+//        udpSocket.send(packet);
+//
+//        System.out.println("Sent a  multicast message.");
+//        System.out.println("Exiting application");
+//        udpSocket.close();
+        
+        UdpServer udpS = new UdpServer(6000, true);
+         
+        
+        while(true){
+           udpS.processRequests(udpS.waitDatagram());
+//           
+//           if(cmd.equals("hearbeat_cliente")){
+//               System.out.println("recebi um heartbeat do cliente");
+//           }
+//           else{
+//               System.out.println("recebi outra coisa qq");
+//           }
+        }
+        
       }
     
     /*public static void main(String[] args)
